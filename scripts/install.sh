918179b2a65d874fd694f47d9ce3f336731e1b78
Alexander Efimov <efimokthefatass@gmail.com>
2022-06-23 15:48:55 +0300

    update readme

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,12 @@
-set -e
+cd "$( dirname "$0" )/.."
+
 # MAIN APP INSTALLATION
 npm i
 npm run build
 
 # MAIN APP AUTOSTART
-echo '/home/pi/bat-ui/dist/linux-armv7l-unpacked/bat-ui' > ~/.xinitrc
-chmod +x ~/.xinitrc
+chmod +x dist/bat-ui*.AppImage
+mkdir -p ~/inenergy-gui/dist
+cp dist/bat-ui*.AppImage ~/inenergy-gui/dist/
+mkdir ~/.inenergy
+echo '~/inengergy-gui/dist/bat-ui*.AppImage > ~/.inenergy/bat-ui.log' > ~/.config/openbox/autostart
